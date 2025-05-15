6ccadf0f62acefa389987af609c7ec90aed7dba5
Hirrua <hirrua.silva20@gmail.com>
2025-05-15 15:36:24 -0300

    first commit

---
+++ src/test/java/com/svg/voluntariado/VoluntariadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.svg.voluntariado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntariadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
